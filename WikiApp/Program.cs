7803905a493cd0264a3680f78d9eca88ef529e0d
Ilya Balashov <ibalashov@dataart.com>
2017-06-30 18:03:46 +0300

    cmd

--- WikiApp/Program.cs
+++ WikiApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.Extensions.CommandLineUtils;
 using SimMetrics.Net.Metric;
 using WikiApp.Models;
 
@@ -7,20 +8,52 @@ namespace WikiApp
 {
     class Program
     {
+        const double Latitude = 37.786971;
+        const double Longitude = -122.399677;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Downloading!");
+            var commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
 
-            Action();
+            CommandOption latitude = commandLineApplication.Option(
+                "-lat | --latitude <value>",
+                "Get wikipedia articles nearest to that latitude",
+                CommandOptionType.SingleValue);
 
-            Console.WriteLine("Done! Press Any Key To Exit...");
+            CommandOption longitude = commandLineApplication.Option(
+                "-lon | --longitude <value>",
+                "Get wikipedia articles nearest to that longitude",
+                CommandOptionType.SingleValue);
+
+            CommandOption verbose = commandLineApplication.Option(
+                "-v | --verbose", "Display debug information.",
+                CommandOptionType.NoValue);
+
+            commandLineApplication.HelpOption("-? | -h | --help");
+
+            commandLineApplication.OnExecute(() =>
+            {
+                if (true)
+                {
+                    Action(verbose.HasValue());
+                }
+                else
+                {
+                    commandLineApplication.ShowHelp();
+                }
+                return 0;
+            });
+
+            commandLineApplication.Execute(args);
+
             Console.ReadLine();
         }
 
 
-        public static void Action()
+        public static void Action(bool verbose)
         {
-            var geopages = WikiApi.WikiApi.Geosearch("37.786971", "-122.399677");
+            var geopages = WikiApi.WikiApi.Geosearch(Latitude, Longitude);
             var metric = new Levenstein();
             
             foreach (var geopage in geopages)
@@ -35,9 +68,10 @@ namespace WikiApp
                     var bestTitle = imagesWithMetrics.Aggregate((image, next) => next.Similarity > image.Similarity ? next : image);
 
                     Console.WriteLine($"\t*[{bestTitle.Ns}] {bestTitle.Title} {bestTitle.Similarity}");
-                    foreach (var image in imagesWithMetrics)
+
+                    if (verbose)
                     {
-                        Console.WriteLine(string.Format("\t[{0}] {1} {2}", image.Ns, image.Title, image.Similarity));
+                        imagesWithMetrics.ForEach(image => Console.WriteLine(string.Format("\t[{0}] {1} {2}", image.Ns, image.Title, image.Similarity)));
                     }
                 }
             }
