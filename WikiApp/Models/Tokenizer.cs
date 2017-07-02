07a30b8c7131ff7c93c878ba609a18fa9a93cbef
Aleksandr Zverev <planerist@gmail.com>
2017-07-02 18:25:40 +0300

    minor optiomizations

--- WikiApp/Models/Tokenizer.cs
+++ WikiApp/Models/Tokenizer.cs
@@ -11,9 +11,10 @@ namespace WikiApp.Models
 
     public class Tokenizer : ITokenizer
     {
+        private static readonly char[] _separators = new[]{' ',':','.'};
         public IEnumerable<string> Tokenize(string input)
         {
-            var words = input.Split(new[]{' ',':','.'}, StringSplitOptions.RemoveEmptyEntries);
+            var words = input.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
             return words.Skip(1).Take(words.Length - 2).Select(x => x.Trim());
         }
     }
